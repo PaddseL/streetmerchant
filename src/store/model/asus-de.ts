1208c759067584b5f82407b6318b073a02a22515
PaddseL <paddsel@gmail.com>
2025-02-04 20:05:48 +0100

    feat(asus-de): update asus-de shop and add rtx 5000 cards

--- src/store/model/asus-de.ts
+++ src/store/model/asus-de.ts
@@ -4,16 +4,24 @@ export const AsusDe: Store = {
   currency: '€',
   labels: {
     inStock: {
-      container: '.buybox--button',
+      container: '#productDetailPageBuyProductForm',
       text: ['in den warenkorb'],
     },
+    maxPrice: {
+      container: '.product-detail-price',
+      euroFormat: true
+    },
+    outOfStock: {
+      container: '.product-detail-delivery-information',
+      text: ['artikel nicht verfügbar']
+    }
   },
   links: [
     {
       brand: 'test:brand',
       model: 'test:model',
       series: 'test:series',
-      url: 'https://webshop.asus.com/de/komponenten/grafikkarten/nvidia-serie/2766/asus-rog-strix-rtx2060s-o8g-evo-v2-gaming',
+      url: 'https://webshop.asus.com/de/Grafikkarten/Dual/ASUS-Dual-GeForce-RTX-4060-EVO-OC-Edition-8GB-GDDR6-Gaming-Grafikkarte/90YV0JC7-M0NA00',
     },
     {
       brand: 'asus',
@@ -80,6 +88,66 @@ export const AsusDe: Store = {
       model: 'tuf oc',
       series: 'rx6700xt',
       url: 'https://webshop.asus.com/de/komponenten/grafikkarten/tuf-247-betrieb/3105/asus-tuf-rx6700xt-o12g-gaming',
+    },
+    {
+      brand: 'asus',
+      model: 'astral',
+      series: '5080',
+      url: 'https://webshop.asus.com/de/Grafikkarten/Nvidia-RTX-5090-RTX-5080/ASUS-ROG-Astral-GeForce-RTX-5080-16GB-GDDR7-Gaming-Grafikkarte/90YV0LV1-M0NA00',
+    },
+    {
+      brand: 'asus',
+      model: 'astral oc',
+      series: '5080',
+      url: 'https://webshop.asus.com/de/Grafikkarten/Nvidia-RTX-5090-RTX-5080/ASUS-ROG-Astral-GeForce-RTX-5080-16GB-GDDR7-OC-Edition-Gaming-Grafikkarte/90YV0LV0-M0NA00',
+    },
+    {
+      brand: 'asus',
+      model: 'prime',
+      series: '5080',
+      url: 'https://webshop.asus.com/de/Grafikkarten/Nvidia-RTX-5090-RTX-5080/ASUS-Prime-GeForce-RTX-5080-16GB-GDDR7-Gaming-Grafikkarte/90YV0LX1-M0NA00',
+    },
+    {
+      brand: 'asus',
+      model: 'tuf',
+      series: '5080',
+      url: 'https://webshop.asus.com/de/Grafikkarten/Nvidia-RTX-5090-RTX-5080/ASUS-TUF-Gaming-GeForce-RTX-5080-16GB-GDDR7-Gaming-Grafikkarte/90YV0M31-M0NA00',
+    },
+    {
+      brand: 'asus',
+      model: 'tuf oc',
+      series: '5080',
+      url: 'https://webshop.asus.com/de/Grafikkarten/Nvidia-RTX-5090-RTX-5080/ASUS-TUF-Gaming-GeForce-RTX-5080-OC-Edition-16GB-GDDR7-Gaming-Grafikkarte/90YV0M30-M0NA00',
+    },
+    {
+      brand: 'asus',
+      model: 'astral',
+      series: '5090',
+      url: 'https://webshop.asus.com/de/Grafikkarten/Nvidia-RTX-5090-RTX-5080/ASUS-ROG-Astral-GeForce-RTX-5090-32GB-GDDR7-Gaming-Grafikkarte/90YV0LW1-M0NA00',
+    },
+    {
+      brand: 'asus',
+      model: 'astral oc',
+      series: '5090',
+      url: 'https://webshop.asus.com/de/Grafikkarten/Nvidia-RTX-5090-RTX-5080/ASUS-ROG-Astral-GeForce-RTX-5090-32GB-GDDR7-OC-Edition-Gaming-Grafikkarte/90YV0LW0-M0NA00',
+    },
+    {
+      brand: 'asus',
+      model: 'astral lc oc',
+      series: '5090',
+      url: 'https://webshop.asus.com/de/Grafikkarten/Nvidia-RTX-5090-RTX-5080/ASUS-ROG-Astral-LC-GeForce-RTX-5090-32GB-GDDR7-OC-Edition-Gaming-Grafikkarte-mit-All-in-One-Fluessig-CPU-Wasserkuehlung/90YV0LW2-M0NA00',
+    },
+    {
+      brand: 'asus',
+      model: 'tuf',
+      series: '5090',
+      url: 'https://webshop.asus.com/de/Grafikkarten/Nvidia-RTX-5090-RTX-5080/ASUS-TUF-Gaming-GeForce-RTX-5090-32GB-GDDR7-Gaming-Grafikkarte/90YV0LY1-M0NA00',
+    },
+    {
+      brand: 'asus',
+      model: 'tuf oc',
+      series: '5090',
+      url: 'https://webshop.asus.com/de/Grafikkarten/Nvidia-RTX-5090-RTX-5080/ASUS-TUF-Gaming-GeForce-RTX-5090-OC-Edition-32GB-GDDR7-Gaming-Grafikkarte/90YV0LY0-M0NA00',
     },
   ],
   name: 'asus-de',
